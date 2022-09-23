6458a1c242a5af25270eb87c7cfcade86c631176
fanzaiyang <zaiyangnihao@163.com>
2022-09-23 10:33:53 +0800

    mp组件

--- breeze-mybatis-plus-spring-boot-starter/src/main/java/cn/fanzy/breeze/mp/config/BreezeMybatisPlusConfig.java
+++ breeze-mybatis-plus-spring-boot-starter/src/main/java/cn/fanzy/breeze/mp/config/BreezeMybatisPlusConfig.java
@@ -5,9 +5,13 @@ import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
+import java.util.List;
+@Slf4j
 @Configuration
 public class BreezeMybatisPlusConfig {
     /**
@@ -21,5 +25,12 @@ public class BreezeMybatisPlusConfig {
         // 乐观锁
         interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
         return interceptor;
+    }
+    /**
+     * 配置检查
+     */
+    @PostConstruct
+    public void checkConfig() {
+        log.info("「微风组件」开启 <注册Mybatis-Plus拦截器> 相关的配置。");
     }
 }
