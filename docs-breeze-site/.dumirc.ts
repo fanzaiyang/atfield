8fe999ba6b52f280b704c34a53568a8f43290943
fanzaiyang <zaiyangnihao@163.com>
2022-12-14 09:13:10 +0800

    重构文档

--- docs-breeze-site/.dumirc.ts
+++ docs-breeze-site/.dumirc.ts
@@ -1,6 +1,7 @@
 import {defineConfig} from 'dumi';
 
 export default defineConfig({
+  base:'/breeze-spring-cloud',
   themeConfig: {
     name: '微风组件',
     logo: '/logo.png',
--- docs/docs__weixin__ma.md.72a17563.async.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkbreeze_spring_cloud=self.webpackChunkbreeze_spring_cloud||[]).push([[510],{55641:function(n,s,_){_.r(s);var i=_(71100),l=_(35532),t=_(80543),m=_(87551),r=_(52919),E=_(67915),d=_(66190),D=_(67294),e=_(85893);function o(){var u=(0,d.eL)(),a=u.texts;return(0,e.jsx)(d.dY,{children:(0,e.jsx)(e.Fragment,{})})}s.default=o}}]);
