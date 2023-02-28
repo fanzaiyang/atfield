a9444060ef6e39bd6447be1d9fac5ff61fdec964
范小柴 <zaiyangnihao@163.com>
2023-02-28 08:25:53 +0800

    fix BreezeCurrentSessionHandler

--- breeze-sqltoy-spring-boot-starter/src/main/java/cn/fanzy/breeze/sqltoy/config/BreezeSqlToyPlusSaTokenConfig.java
+++ breeze-sqltoy-spring-boot-starter/src/main/java/cn/fanzy/breeze/sqltoy/config/BreezeSqlToyPlusSaTokenConfig.java
@@ -6,6 +6,7 @@ import cn.fanzy.breeze.sqltoy.plus.session.BreezeSaTokenCurrentSessionHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +16,7 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnClass(StpUtil.class)
 public class BreezeSqlToyPlusSaTokenConfig {
     @Bean
+    @ConditionalOnMissingBean
     public BreezeCurrentSessionHandler breezeCurrentSessionHandler() {
         return new BreezeSaTokenCurrentSessionHandler();
     }
