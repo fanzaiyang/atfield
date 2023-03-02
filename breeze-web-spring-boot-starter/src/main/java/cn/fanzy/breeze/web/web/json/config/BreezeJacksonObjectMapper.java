5a3a4b95bb2d9fa8ae96011138109b46f697a1ef
范小柴 <zaiyangnihao@163.com>
2023-03-02 11:08:08 +0800

    update doc

--- breeze-web-spring-boot-starter/src/main/java/cn/fanzy/breeze/web/web/json/config/BreezeJacksonObjectMapper.java
+++ breeze-web-spring-boot-starter/src/main/java/cn/fanzy/breeze/web/web/json/config/BreezeJacksonObjectMapper.java
@@ -4,23 +4,49 @@ import cn.fanzy.breeze.web.web.json.jackson.BreezeBeanSerializerModifier;
 import cn.fanzy.breeze.web.web.json.jackson.BreezeCustomizeNullJsonSerializer;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
 
 public class BreezeJacksonObjectMapper extends ObjectMapper {
     private static final long serialVersionUID = 4349248944480408489L;
-
-    public BreezeJacksonObjectMapper() {
+    public BreezeJacksonObjectMapper(String dateFormat) {
         super();
         // 收到未知属性时不报异常
         this.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
-        // 反序列化时，属性不存在的兼容处理
-        this.getDeserializationConfig()
-                .withoutFeatures(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        this.registerModule(new JavaTimeModule());
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(Long.class, com.fasterxml.jackson.databind.ser.std.ToStringSerializer.instance);
+        simpleModule.addSerializer(Long.TYPE, com.fasterxml.jackson.databind.ser.std.ToStringSerializer.instance);
+        //反序列化的时候如果多了其他属性,不抛出异常
+        this.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        // 日期禁止时间戳
+        this.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        //日期序列化
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(dateFormat)));
+        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern("HH:mm:ss")));
+        //日期反序列化
+        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(dateFormat)));
+        javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        javaTimeModule.addDeserializer(LocalTime.class, new LocalTimeDeserializer(DateTimeFormatter.ofPattern("HH:mm:ss")));
         this.setSerializerFactory(this.getSerializerFactory()
                 .withSerializerModifier(new BreezeBeanSerializerModifier()));
-        this.getSerializerProvider().setNullValueSerializer(new BreezeCustomizeNullJsonSerializer.NullObjectJsonSerializer());
+        this.getSerializerProvider()
+                .setNullValueSerializer(new BreezeCustomizeNullJsonSerializer.NullObjectJsonSerializer());
+        this.registerModules(simpleModule, javaTimeModule);
     }
 }
--- breeze-web-spring-boot-starter/src/main/java/cn/fanzy/breeze/web/web/json/config/BreezeJacksonWebConfig.java
+++ breeze-web-spring-boot-starter/src/main/java/cn/fanzy/breeze/web/web/json/config/BreezeJacksonWebConfig.java
@@ -1,20 +1,22 @@
 package cn.fanzy.breeze.web.web.json.config;
 
 import cn.fanzy.breeze.web.web.json.properties.BreezeWebJsonProperties;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.PostConstruct;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 /**
  * <pre>
@@ -30,38 +32,23 @@ import javax.annotation.PostConstruct;
  */
 @Slf4j
 @Configuration
+@RequiredArgsConstructor
 @AutoConfigureBefore(JacksonAutoConfiguration.class)
 @EnableConfigurationProperties({BreezeWebJsonProperties.class})
-@ConditionalOnProperty(prefix = "breeze.web.json", name = {"enable"}, havingValue = "true",matchIfMissing = true)
-public class BreezeJacksonWebConfig {
+@ConditionalOnProperty(prefix = "breeze.web.json", name = {"enable"}, havingValue = "true", matchIfMissing = true)
+public class BreezeJacksonWebConfig implements WebMvcConfigurer {
+    @Value("${spring.jackson.date-format}")
+    private String jacksonDateFormat;
 
-    @Value("${spring.mvc.format.date-time:yyyy-MM-dd HH:mm:ss}")
-    private String dateTimeFormat;
-    @Bean
-    @Primary
-    @ConditionalOnMissingBean
-    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
-        return new BreezeJacksonObjectMapper();
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.removeIf(MappingJackson2HttpMessageConverter.class::isInstance);
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(new BreezeJacksonObjectMapper(jacksonDateFormat));
+        converters.add(0, converter);
+        converters.add(new StringHttpMessageConverter(StandardCharsets.UTF_8));
     }
 
-//    @Bean
-//    MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
-//        log.info("mappingJackson2HttpMessageConverter....");
-//        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        //现在需要在中间加一个
-//        objectMapper.registerModule(new JavaTimeModule());
-//        objectMapper.setDateFormat(new SimpleDateFormat(dateTimeFormat));
-//        /** 为objectMapper注册一个带有SerializerModifier的Factory */
-//        objectMapper.setSerializerFactory(objectMapper.getSerializerFactory()
-//                .withSerializerModifier(new BreezeBeanSerializerModifier()));
-//        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
-//        SerializerProvider serializerProvider = objectMapper.getSerializerProvider();
-//        serializerProvider.setNullValueSerializer(new BreezeNullValueSerializer());
-//
-//        mappingJackson2HttpMessageConverter.setObjectMapper(objectMapper);  // 设置objectMapper
-//        return mappingJackson2HttpMessageConverter;
-//    }
     @PostConstruct
     public void checkConfig() {
         log.info("「微风组件」开启 <JSON转换器> 相关的配置。");
