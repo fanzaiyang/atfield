07c88a8cd0e3e792d48307966ee73968fa95aeb1
范小柴 <zaiyangnihao@163.com>
2023-04-24 12:05:01 +0800

    添加属性

--- breeze-web-spring-boot-starter/src/main/java/cn/fanzy/breeze/web/logs/aop/BreezeLogsAop.java
+++ breeze-web-spring-boot-starter/src/main/java/cn/fanzy/breeze/web/logs/aop/BreezeLogsAop.java
@@ -65,7 +65,7 @@ public class BreezeLogsAop {
         Map<String, Object> requestData = JoinPointUtils.getParams(joinPoint);
         String clientIp = SpringUtils.getClientIp();
         log.info("===客户端IP：{}，请求地址：「{}」，\n请求参数：{}", clientIp, SpringUtils.getRequestUri(), JSONUtil.toJsonStr(SpringUtils.getRequestParams()));
-        if (annotation != null) {
+        if (annotation != null && annotation.ignore()) {
             breezeRequestArgs = new BreezeRequestArgs();
             breezeRequestArgs.setIgnore(annotation.ignore());
             return;
