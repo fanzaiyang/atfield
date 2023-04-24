be218df041e82e6e65b90426c286995095919d0d
范小柴 <zaiyangnihao@163.com>
2023-04-24 11:13:58 +0800

    fix:Date格式化问题

--- breeze-web-spring-boot-starter/src/main/java/cn/fanzy/breeze/web/web/json/config/BreezeJacksonObjectMapper.java
+++ breeze-web-spring-boot-starter/src/main/java/cn/fanzy/breeze/web/web/json/config/BreezeJacksonObjectMapper.java
@@ -2,10 +2,13 @@ package cn.fanzy.breeze.web.web.json.config;
 
 import cn.fanzy.breeze.web.web.json.jackson.BreezeBeanSerializerModifier;
 import cn.fanzy.breeze.web.web.json.jackson.BreezeCustomizeNullJsonSerializer;
+import cn.hutool.core.util.StrUtil;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.deser.std.DateDeserializers;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.DateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
@@ -14,13 +17,21 @@ import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
 
+/**
+ * 微风杰克逊对象映射器
+ *
+ * @author fanzaiyang
+ * @date 2023-04-24
+ */
 public class BreezeJacksonObjectMapper extends ObjectMapper {
     private static final long serialVersionUID = 4349248944480408489L;
     public BreezeJacksonObjectMapper(String jacksonDateFormat,String mvcDateFormat, String mvcTimeFormat) {
@@ -36,10 +47,12 @@ public class BreezeJacksonObjectMapper extends ObjectMapper {
         this.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         JavaTimeModule javaTimeModule = new JavaTimeModule();
         //日期序列化
+        javaTimeModule.addSerializer(Date.class, new DateSerializer(false,new SimpleDateFormat(StrUtil.blankToDefault(jacksonDateFormat,"yyyy-MM-dd HH:mm:ss"))));
         javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(jacksonDateFormat)));
         javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern(mvcDateFormat)));
         javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern(mvcTimeFormat)));
         //日期反序列化
+        javaTimeModule.addDeserializer(Date.class, new DateDeserializers.DateDeserializer());
         javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(jacksonDateFormat)));
         javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern(mvcDateFormat)));
         javaTimeModule.addDeserializer(LocalTime.class, new LocalTimeDeserializer(DateTimeFormatter.ofPattern(mvcTimeFormat)));
@@ -48,6 +61,8 @@ public class BreezeJacksonObjectMapper extends ObjectMapper {
         this.getSerializerProvider()
                 .setNullValueSerializer(new BreezeCustomizeNullJsonSerializer.NullObjectJsonSerializer());
         this.registerModules(simpleModule, javaTimeModule);
+        String format = StrUtil.blankToDefault(jacksonDateFormat,mvcDateFormat);
+        this.setDateFormat(new SimpleDateFormat(StrUtil.blankToDefault(format,"yyyy-MM-dd HH:mm:ss")));
     }
 
 }
