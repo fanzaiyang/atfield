0521a1d7e7f416e17d295f20bd34fd86756c36ec
范小柴 <zaiyangnihao@163.com>
2023-05-29 15:41:49 +0800

    更新文档

--- breeze-wechat/breeze-wechat-pay-spring-boot-starter/src/main/java/cn/fanzy/breeze/wechat/pay/config/WxPayConfiguration.java
+++ breeze-wechat/breeze-wechat-pay-spring-boot-starter/src/main/java/cn/fanzy/breeze/wechat/pay/config/WxPayConfiguration.java
@@ -1,12 +1,12 @@
 package cn.fanzy.breeze.wechat.pay.config;
 
 import cn.fanzy.breeze.wechat.pay.properties.WxPayProperties;
-import cn.hutool.core.util.StrUtil;
 import com.github.binarywang.wxpay.config.WxPayConfig;
 import com.github.binarywang.wxpay.service.WxPayService;
 import com.github.binarywang.wxpay.service.impl.WxPayServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import me.chanjar.weixin.common.error.WxRuntimeException;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -44,12 +44,20 @@ public class WxPayConfiguration {
             throw new WxRuntimeException("大哥，拜托先看下项目首页的说明（readme文件），添加下相关配置，注意别配错了！");
         }
         WxPayConfig payConfig = new WxPayConfig();
-        payConfig.setAppId(StrUtil.trimToNull(this.properties.getAppId()));
-        payConfig.setMchId(StrUtil.trimToNull(this.properties.getMchId()));
-        payConfig.setMchKey(StrUtil.trimToNull(this.properties.getMchKey()));
-        payConfig.setSubAppId(StrUtil.trimToNull(this.properties.getSubAppId()));
-        payConfig.setSubMchId(StrUtil.trimToNull(this.properties.getSubMchId()));
-        payConfig.setKeyPath(StrUtil.trimToNull(this.properties.getKeyPath()));
+        payConfig.setAppId(StringUtils.trimToNull(this.properties.getAppId()));
+        payConfig.setMchId(StringUtils.trimToNull(this.properties.getMchId()));
+        payConfig.setMchKey(StringUtils.trimToNull(this.properties.getMchKey()));
+        payConfig.setSubAppId(StringUtils.trimToNull(this.properties.getSubAppId()));
+        payConfig.setSubMchId(StringUtils.trimToNull(this.properties.getSubMchId()));
+        payConfig.setKeyPath(StringUtils.trimToNull(this.properties.getKeyPath()));
+        //以下是apiv3以及支付分相关
+        payConfig.setServiceId(StringUtils.trimToNull(this.properties.getServiceId()));
+        payConfig.setPayScoreNotifyUrl(StringUtils.trimToNull(this.properties.getPayScoreNotifyUrl()));
+        payConfig.setPrivateKeyPath(StringUtils.trimToNull(this.properties.getPrivateKeyPath()));
+        payConfig.setPrivateCertPath(StringUtils.trimToNull(this.properties.getPrivateCertPath()));
+        payConfig.setCertSerialNo(StringUtils.trimToNull(this.properties.getCertSerialNo()));
+        payConfig.setApiV3Key(StringUtils.trimToNull(this.properties.getApiv3Key()));
+
         // 可以指定是否使用沙箱环境
         payConfig.setUseSandboxEnv(this.properties.isUseSandboxEnv());
         mpServices = new WxPayServiceImpl();
--- breeze-wechat/breeze-wechat-pay-spring-boot-starter/src/main/java/cn/fanzy/breeze/wechat/pay/properties/WxPayProperties.java
+++ breeze-wechat/breeze-wechat-pay-spring-boot-starter/src/main/java/cn/fanzy/breeze/wechat/pay/properties/WxPayProperties.java
@@ -14,37 +14,64 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "breeze.wechat.pay")
 public class WxPayProperties {
     /**
-     * 设置微信公众号或者小程序等的appid
+     * 设置微信公众号或者小程序等的appid.
      */
     private String appId;
 
     /**
-     * 微信支付商户号
+     * 微信支付商户号.
      */
     private String mchId;
 
     /**
-     * 微信支付商户密钥
+     * 微信支付商户密钥.
      */
     private String mchKey;
 
     /**
-     * 服务商模式下的子商户公众账号ID，普通模式请不要配置，请在配置文件中将对应项删除
+     * 服务商模式下的子商户公众账号ID，普通模式请不要配置，请在配置文件中将对应项删除.
      */
     private String subAppId;
 
     /**
-     * 服务商模式下的子商户号，普通模式请不要配置，最好是请在配置文件中将对应项删除
+     * 服务商模式下的子商户号，普通模式请不要配置，最好是请在配置文件中将对应项删除.
      */
     private String subMchId;
 
     /**
-     * apiclient_cert.p12文件的绝对路径，或者如果放在项目中，请以classpath:开头指定
+     * apiclient_cert.p12文件的绝对路径，或者如果放在项目中，请以classpath:开头指定.
      */
     private String keyPath;
 
     /**
-     * 以指定是否使用沙箱环境,默认false
+     * 微信支付分serviceId
      */
-    private boolean useSandboxEnv = false;
+    private String serviceId;
+
+    /**
+     * 证书序列号
+     */
+    private String certSerialNo;
+
+    /**
+     * apiV3秘钥
+     */
+    private String apiv3Key;
+
+    /**
+     * 微信支付分回调地址
+     */
+    private String payScoreNotifyUrl;
+
+    /**
+     * apiv3 商户apiclient_key.pem
+     */
+    private String privateKeyPath;
+
+    /**
+     * apiv3 商户apiclient_cert.pem
+     */
+    private String privateCertPath;
+
+    private boolean useSandboxEnv;
 }
