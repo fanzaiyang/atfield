7d2fda7f9c3b6f92168dab784e69965e147f2687
范小柴 <zaiyangnihao@163.com>
2023-07-27 09:23:49 +0800

    feature(升级版本): 2.2.6 - 移除扩展的部门服务 - 迁移代码到本地

--- breeze-wechat/breeze-wechat-cp-spring-boot-starter/src/main/java/me/chanjar/weixin/cp/bean/WxCpDepart.java
+++ breeze-wechat/breeze-wechat-cp-spring-boot-starter/src/main/java/me/chanjar/weixin/cp/bean/WxCpDepart.java
@@ -14,11 +14,27 @@ import java.io.Serializable;
 public class WxCpDepart implements Serializable {
   private static final long serialVersionUID = -5028321625140879571L;
 
+  /**
+   * 部门id，32位整型，指定时必须大于1。若不填该参数，将自动生成id
+   * 1~2^31 - 1之间
+   */
   private Long id;
+  /**
+   * 部门名称。同一个层级的部门名称不能重复。长度限制为1~32个UTF-8字符，字符不能包括:*?"<>｜
+   */
   private String name;
+  /**
+   * 英文名称。同一个层级的部门名称不能重复。需要在管理后台开启多语言支持才能生效。长度限制为1~64个字符，字符不能包括:*?"<>｜
+   */
   private String enName;
   private String[] departmentLeader;
+  /**
+   * 父部门id，32位整型1~2^31 - 1之间
+   */
   private Long parentId;
+  /**
+   * 在父部门中的次序值。order值大的排序靠前。有效的值范围是[0, 2^32)
+   */
   private Long order;
 
   /**
