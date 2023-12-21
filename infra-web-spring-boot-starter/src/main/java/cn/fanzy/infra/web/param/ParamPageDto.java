b3ca77bbe2e4272f15de3aa74746bf91106c8294
fanzaiyang <zaiyangnihao@163.com>
2023-12-21 17:43:48 +0800

    doc

--- infra-web-spring-boot-starter/src/main/java/cn/fanzy/infra/web/param/ParamPageDto.java
+++ infra-web-spring-boot-starter/src/main/java/cn/fanzy/infra/web/param/ParamPageDto.java
@@ -21,13 +21,19 @@ public class ParamPageDto implements Serializable {
 
     @Serial
     private static final long serialVersionUID = -1241624388851356884L;
+    /**
+     * 当前页，默认：1
+     */
     private Integer pageNum;
+    /**
+     * 每页个数，默认：20
+     */
     private Integer pageSize;
 
     public Integer getPageNum() {
         if (pageNum == null || pageNum < 1) {
             String pageNum = SpringUtils.getRequest().getParameter("pageNum");
-            if(StrUtil.isBlank(pageNum)){
+            if (StrUtil.isBlank(pageNum)) {
                 pageNum = SpringUtils.getRequest().getParameter("pageNo");
             }
             return NumberUtil.isNumber(pageNum) ? NumberUtil.parseInt(pageNum) : 1;
@@ -38,7 +44,7 @@ public class ParamPageDto implements Serializable {
     public Integer getPageSize() {
         if (pageSize == null || pageSize < 1) {
             String pageSize = SpringUtils.getRequest().getParameter("pageSize");
-            return NumberUtil.isNumber(pageSize) ? NumberUtil.parseInt(pageSize) : 15;
+            return NumberUtil.isNumber(pageSize) ? NumberUtil.parseInt(pageSize) : 20;
         }
         return pageSize;
     }
