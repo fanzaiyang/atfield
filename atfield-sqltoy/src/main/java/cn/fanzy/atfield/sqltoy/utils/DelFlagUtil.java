7848388e195ade01479e13740727bb3d466d1efa
范小柴 <zaiyangnihao@163.com>
2024-01-21 22:00:05 +0800

    添加方法

---
+++ atfield-sqltoy/src/main/java/cn/fanzy/atfield/sqltoy/utils/DelFlagUtil.java
@@ -0,0 +1,49 @@
+package cn.fanzy.atfield.sqltoy.utils;
+
+import cn.fanzy.atfield.core.spring.SpringUtils;
+import cn.fanzy.atfield.sqltoy.property.SqltoyExtraProperties;
+
+/**
+ * 删除标识工具类
+ *
+ * @author fanzaiyang
+ * @date 2024-01-21
+ */
+public class DelFlagUtil {
+
+    /**
+     * 得到配置
+     *
+     * @return {@link SqltoyExtraProperties}
+     */
+    public static SqltoyExtraProperties getProperties() {
+        return SpringUtils.getBean(SqltoyExtraProperties.class);
+    }
+
+    /**
+     * 得到逻辑删除的值
+     *
+     * @return {@link String}
+     */
+    public static String getLogicDeleteValue() {
+        return getProperties().getLogicDeleteValue();
+    }
+
+    /**
+     * 得到未删除的值
+     *
+     * @return {@link String}
+     */
+    public static String getLogicNotDeleteValue() {
+        return getProperties().getLogicNotDeleteValue();
+    }
+
+    /**
+     * 得到逻辑删除田
+     *
+     * @return {@link String}
+     */
+    public static String getLogicDeleteField() {
+        return getProperties().getLogicDeleteField();
+    }
+}
