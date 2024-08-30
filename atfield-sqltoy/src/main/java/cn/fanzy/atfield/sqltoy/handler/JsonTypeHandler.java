07df877ded80c3c6761ff009f0c5008f4f686aac
fanzaiyang <zaiyangnihao@163.com>
2024-08-30 15:12:51 +0800

    pref(sqltoy) 兼容集合和对象

--- atfield-sqltoy/src/main/java/cn/fanzy/atfield/sqltoy/handler/JsonTypeHandler.java
+++ atfield-sqltoy/src/main/java/cn/fanzy/atfield/sqltoy/handler/JsonTypeHandler.java
@@ -1,12 +1,15 @@
 package cn.fanzy.atfield.sqltoy.handler;
 
+import cn.hutool.core.util.ClassUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.sagacity.sqltoy.plugins.TypeHandler;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Types;
+import java.util.Collection;
 
 /**
  * JSON 类型处理程序
@@ -14,11 +17,13 @@ import java.sql.Types;
  * @author fanzaiyang
  * @date 2024/02/21
  */
+@Slf4j
 public class JsonTypeHandler extends TypeHandler {
 
     @Override
     public boolean setValue(Integer dbType, PreparedStatement pst, int paramIndex, int jdbcType, Object value) throws SQLException {
-        if (jdbcType == Types.ARRAY) {
+        if (jdbcType == Types.ARRAY ||
+            jdbcType == Types.JAVA_OBJECT) {
             pst.setString(paramIndex, JSONUtil.toJsonStr(value));
             return true;
         }
@@ -27,14 +32,25 @@ public class JsonTypeHandler extends TypeHandler {
 
     @Override
     public Object toJavaType(String javaTypeName, Class genericType, Object jdbcValue) throws Exception {
-        if ("java.util.List".equalsIgnoreCase(javaTypeName) && genericType != null) {
-//            JSONConfig config = new JSONConfig();
-//            config.setDateFormat("yyyy-MM-dd HH:mm:ss");
-//            config.setIgnoreNullValue(true);
-//            config.setWriteLongAsString(true);
+        log.info("转为Java类型：javaTypeName:{}", jdbcValue);
+
+        Class<?> clazz = Class.forName(javaTypeName);
+        if (Collection.class.isAssignableFrom(clazz) || clazz.isArray()) {
             String value = StrUtil.blankToDefault(jdbcValue.toString(), "[]");
-            return JSONUtil.toList(value, genericType);
+            return JSONUtil.toList(value, genericType == null ? Object.class : genericType);
         }
+        if (ClassUtil.isNormalClass(clazz)) {
+            return JSONUtil.toBean(jdbcValue.toString(), clazz);
+        }
+        if (jdbcValue instanceof String) {
+            if (jdbcValue.toString().startsWith("{") && jdbcValue.toString().endsWith("}")) {
+                return JSONUtil.toBean(jdbcValue.toString(), clazz);
+            }
+            if (jdbcValue.toString().startsWith("[") && jdbcValue.toString().endsWith("]")) {
+                return JSONUtil.toList(jdbcValue.toString(), genericType == null ? Object.class : genericType);
+            }
+        }
+
         return super.toJavaType(javaTypeName, genericType, jdbcValue);
     }
 }
