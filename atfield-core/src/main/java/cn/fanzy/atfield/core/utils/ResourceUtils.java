f9870979ccea63105e1eb5b3f66c624a0a9214a5
fanzaiyang <zaiyangnihao@163.com>
2024-09-25 11:49:10 +0800

    添加资源文件读取方式

--- atfield-core/src/main/java/cn/fanzy/atfield/core/utils/ResourceUtils.java
+++ atfield-core/src/main/java/cn/fanzy/atfield/core/utils/ResourceUtils.java
@@ -23,7 +23,7 @@ public class ResourceUtils extends org.springframework.util.ResourceUtils {
      * 将资源作为流获取
      * <pre>
      *     返回流的目的：在jar文件中，不能直接通过文件资源路径拿到文件，但是可以在jar包中拿到文件流。
-     *     2. 待优化：把resource下文件放入缓存中，提高性能。
+     *     ⚠️待优化：把resource下文件放入缓存中，提高性能。
      * </pre>
      *
      * @param relativeFilePath 相对文件路径
