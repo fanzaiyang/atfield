ff1d6369451a72571beeb2a827b9da5a2781c9d6
fanzaiyang <zaiyangnihao@163.com>
2024-10-10 10:56:40 +0800

    pref(web) @Scheduled实现多线程并发定时任务

--- atfield-web/src/main/java/cn/fanzy/atfield/web/configuration/TaskMultiThreadConfig.java
+++ atfield-web/src/main/java/cn/fanzy/atfield/web/configuration/TaskMultiThreadConfig.java
@@ -38,24 +38,24 @@ public class TaskMultiThreadConfig {
         int corePoolSize = cpuNum * 2 + 1;
         // 线程池的最大线程数
         int maximumPoolSize = cpuNum * 5;
-        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         // 核心线程数，默认为1
-        threadPoolTaskExecutor.setCorePoolSize(property.getCorePoolSize() == null ? corePoolSize : property.getCorePoolSize());
+        executor.setCorePoolSize(property.getCorePoolSize() == null ? corePoolSize : property.getCorePoolSize());
         // 最大线程数，默认为Integer.MAX_VALUE
-        threadPoolTaskExecutor.setMaxPoolSize(property.getMaxPoolSize() == null ? maximumPoolSize : property.getMaxPoolSize());
+        executor.setMaxPoolSize(property.getMaxPoolSize() == null ? maximumPoolSize : property.getMaxPoolSize());
         // 队列最大长度，一般需要设置值: 大于等于notifyScheduledMainExecutor.maxNum；默认为Integer.MAX_VALUE
-        threadPoolTaskExecutor.setQueueCapacity(property.getQueueCapacity() == null ? 50 : property.getQueueCapacity());
+        executor.setQueueCapacity(property.getQueueCapacity() == null ? 50 : property.getQueueCapacity());
         // 线程池维护线程所允许的空闲时间，默认为60s
-        threadPoolTaskExecutor.setKeepAliveSeconds(property.getKeepAliveSeconds() == null ? 60 : property.getKeepAliveSeconds());
+        executor.setKeepAliveSeconds(property.getKeepAliveSeconds() == null ? 60 : property.getKeepAliveSeconds());
         // 线程池对拒绝任务（无线程可用）的处理策略，目前只支持AbortPolicy、CallerRunsPolicy；默认为后者
         // AbortPolicy:直接抛出java.util.concurrent.RejectedExecutionException异常
         // CallerRunsPolicy:主线程直接执行该任务，执行完之后尝试添加下一个任务到线程池中，可以有效降低向线程池内添加任务的速度
         // DiscardOldestPolicy:抛弃旧的任务、暂不支持；会导致被丢弃的任务无法再次被执行
         // DiscardPolicy:抛弃当前任务、暂不支持；会导致被丢弃的任务无法再次被执行
-        threadPoolTaskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
-        threadPoolTaskExecutor.setThreadNamePrefix(StrUtil.blankToDefault(property.getThreadNamePrefix(),"task-thread-"));
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.setThreadNamePrefix(StrUtil.blankToDefault(property.getThreadNamePrefix(),"task-thread-"));
         //执行初始化会自动执行afterPropertiesSet()初始化
-        threadPoolTaskExecutor.initialize();
-        return threadPoolTaskExecutor;
+        executor.initialize();
+        return executor;
     }
 }
