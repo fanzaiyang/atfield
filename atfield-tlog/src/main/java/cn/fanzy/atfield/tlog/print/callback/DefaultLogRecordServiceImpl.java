e27b26a44e753b99ffff64da5a3169998f1973a7
fanzaiyang <zaiyangnihao@163.com>
2024-10-17 17:21:11 +0800

    pref(log) 操作类型

--- atfield-tlog/src/main/java/cn/fanzy/atfield/tlog/print/callback/DefaultLogRecordServiceImpl.java
+++ atfield-tlog/src/main/java/cn/fanzy/atfield/tlog/print/callback/DefaultLogRecordServiceImpl.java
@@ -2,10 +2,12 @@ package cn.fanzy.atfield.tlog.print.callback;
 
 
 import cn.fanzy.atfield.tlog.print.bean.LogRecordInfo;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class DefaultLogRecordServiceImpl implements LogRecordService {
     @Override
     public void write(LogRecordInfo record) {
-
+        log.info("{} - {} - {} - {}", record.getOperatorName(), record.getOperateType(), record.getContent(), record.getOperateTime());
     }
 }
