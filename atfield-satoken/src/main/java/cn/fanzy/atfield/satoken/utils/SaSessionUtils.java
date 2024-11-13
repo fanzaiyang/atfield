bc088caadba68ca236f307a503a0a4db735f8336
fanzaiyang <zaiyangnihao@163.com>
2024-11-13 14:34:28 +0800

    pref(sa-token) 添加session工具类

---
+++ atfield-satoken/src/main/java/cn/fanzy/atfield/satoken/utils/SaSessionUtils.java
@@ -0,0 +1,46 @@
+package cn.fanzy.atfield.satoken.utils;
+
+import cn.dev33.satoken.session.SaSession;
+import cn.dev33.satoken.stp.StpUtil;
+import cn.fanzy.atfield.core.utils.ObjectUtils;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+
+import java.util.List;
+
+
+public class SaSessionUtils {
+
+    public static void set(String key, Object value) {
+        SaSession saSession = StpUtil.getSession();
+        saSession.set(key, JSONUtil.toJsonStr(value));
+    }
+
+    /**
+     * 获取模型
+     *
+     * @param key   钥匙
+     * @param clazz 克拉兹
+     * @return {@link T }
+     */
+    public static <T> T getModel(String key, Class<T> clazz) {
+        SaSession saSession = StpUtil.getSession();
+        return ObjectUtils.cast(saSession.get(key), clazz);
+    }
+
+    /**
+     * 获取列表
+     *
+     * @param key   钥匙
+     * @param clazz 克拉兹
+     * @return {@link List }<{@link T }>
+     */
+    public static <T> List<T> getList(String key, Class<T> clazz) {
+        SaSession saSession = StpUtil.getSession();
+        String object = saSession.getString(key);
+        if (StrUtil.isNotBlank(object)) {
+            return JSONUtil.toList(object, clazz);
+        }
+        return List.of();
+    }
+}
