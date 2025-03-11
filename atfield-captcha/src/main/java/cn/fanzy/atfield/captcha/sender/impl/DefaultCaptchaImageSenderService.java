58a2228b273da580c7c2e4f84d0905ce23493475
fanzaiyang <zaiyangnihao@163.com>
2025-03-11 17:20:29 +0800

    pref(sqltoy) 逻辑删除增强1.1.10

--- atfield-captcha/src/main/java/cn/fanzy/atfield/captcha/sender/impl/DefaultCaptchaImageSenderService.java
+++ atfield-captcha/src/main/java/cn/fanzy/atfield/captcha/sender/impl/DefaultCaptchaImageSenderService.java
@@ -4,6 +4,7 @@ import cn.fanzy.atfield.captcha.bean.CaptchaCode;
 import cn.fanzy.atfield.captcha.bean.CaptchaImageCodeInfo;
 import cn.fanzy.atfield.captcha.sender.CaptchaImageSenderService;
 import cn.fanzy.atfield.core.spring.SpringUtils;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -24,7 +25,18 @@ public class DefaultCaptchaImageSenderService extends CaptchaImageSenderService 
         try {
             CaptchaImageCodeInfo code = (CaptchaImageCodeInfo) codeInfo;
             log.debug("【图形验证码发送器】向客户端 {} 发送验证码，验证码的内容为 {} ", target, codeInfo.getCode());
-            ImageIO.write(code.getImage(), "PNG", SpringUtils.getResponse().getOutputStream());
+            // 计算验证码图片的宽度和高度
+            int width = code.getImage().getWidth();
+            int height = code.getImage().getHeight();
+            // 获取每个像素的字节数
+            int bitsPerPixel = code.getImage().getColorModel().getPixelSize();
+            int bytesPerPixel = (bitsPerPixel + 7) / 8;
+            // 计算图像占用的内存大小
+            int memorySize = width * height * bytesPerPixel;
+            HttpServletResponse response = SpringUtils.getResponse();
+            response.setContentLength(memorySize);
+            response.setContentType("image/png");
+            ImageIO.write(code.getImage(), "PNG", response.getOutputStream());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
