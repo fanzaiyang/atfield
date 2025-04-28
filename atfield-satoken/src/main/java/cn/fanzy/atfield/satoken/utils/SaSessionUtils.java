61469275646a65ff68583d376b338281eea0c6a5
fanzaiyang <zaiyangnihao@163.com>
2025-04-28 17:26:12 +0800

    pref(satoken) satoken用户

--- atfield-satoken/src/main/java/cn/fanzy/atfield/satoken/utils/SaSessionUtils.java
+++ atfield-satoken/src/main/java/cn/fanzy/atfield/satoken/utils/SaSessionUtils.java
@@ -3,6 +3,7 @@ package cn.fanzy.atfield.satoken.utils;
 import cn.dev33.satoken.session.SaSession;
 import cn.dev33.satoken.stp.StpUtil;
 import cn.fanzy.atfield.core.utils.ObjectUtils;
+import cn.hutool.core.lang.Assert;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 
@@ -16,7 +17,7 @@ import java.util.List;
  * @date 2024/11/13
  */
 public class SaSessionUtils {
-    public static final String SA_LOGIN_NAME = "sa_login_name";
+    public static final String SA_LOGIN_NAME = SaSession.USER + "_NAME";
 
     /**
      * 设置
@@ -26,6 +27,7 @@ public class SaSessionUtils {
      */
     public static void set(String key, Object value) {
         SaSession saSession = StpUtil.getSession();
+        Assert.notNull(saSession, "Session is null");
         if (value instanceof String) {
             if (JSONUtil.isTypeJSON(value.toString())) {
                 saSession.set(key, value);
@@ -44,6 +46,7 @@ public class SaSessionUtils {
      */
     public static <T> T getModel(String key, Class<T> clazz) {
         SaSession saSession = StpUtil.getSession();
+        Assert.notNull(saSession, "Session is null");
         return ObjectUtils.cast(saSession.get(key), clazz);
     }
 
@@ -55,6 +58,9 @@ public class SaSessionUtils {
      */
     public static String getStr(String key) {
         SaSession saSession = StpUtil.getSession();
+        if (saSession == null) {
+            return null;
+        }
         return saSession.getString(key);
     }
 
@@ -67,6 +73,9 @@ public class SaSessionUtils {
      */
     public static <T> List<T> getList(String key, Class<T> clazz) {
         SaSession saSession = StpUtil.getSession();
+        if (saSession == null) {
+            return List.of();
+        }
         String object = saSession.getString(key);
         if (StrUtil.isNotBlank(object)) {
             return JSONUtil.toList(object, clazz);
